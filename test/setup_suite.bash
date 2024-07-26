eb357a92acc2acda9830eaaa83b293ee607bb54f
Victor Fonseca Vega <vrfvega@gmail.com>
2024-07-26 19:15:57 -0400

    Add env variable to ci.yml

--- test/setup_suite.bash
+++ test/setup_suite.bash
@@ -1,16 +1,16 @@
 setup_suite() {
   docker compose up -d
   
-  # sleep 25
+  sleep 25
 
-  # while [ true ] 
-  # do 
-  #   if docker compose logs | grep -q 'RCON running'
-  #     then 
-  #       break;
-  #   fi
-  #   sleep 1 
-  # done
+  while [ true ] 
+  do 
+    if docker compose logs | grep -q 'RCON running'
+      then 
+        break;
+    fi
+    sleep 1 
+  done
 }
 
 teardown_suite() {
