f39dc08dcd4be2dc19016d965d1d743db561225e
Victor Fonseca Vega <vrfvega@gmail.com>
2024-07-26 13:15:57 -0400

    Implement validation test suite and CI action

---
+++ test/setup_suite.bash
@@ -0,0 +1,15 @@
+setup_suite() {
+  docker compose up -d
+  while [ true ] 
+  do 
+    if docker compose logs | grep -q 'RCON running'
+      then 
+        break;
+    fi
+    sleep 1 
+  done
+}
+
+teardown_suite() {
+  docker compose down -v --remove-orphans
+}
